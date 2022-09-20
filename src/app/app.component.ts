dc4541564442276c594837502a639dbfbe496493
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2022-09-20 22:15:52 +0300

    add basic kendo ui grid component. add get and put http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'kendo-angular-app';
+export class AppComponent implements OnInit {
+  public gridData: any[] = [];
+  public checked: boolean = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any>('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      .subscribe((data: any) => {
+        this.gridData = data;
+      });
+  }
+
+  edit() {
+    const body = {
+      userId: 1,
+      id: 3,
+      title: 'Teodora',
+      body: 'Zhel',
+    };
+    this.http
+      .put<any>(`https://jsonplaceholder.typicode.com/posts/${body.id}`, body)
+      .subscribe((data) => {
+        const index = this.gridData.findIndex(entry => entry.id === body.id); // 2
+        this.gridData[index] = data;
+        console.log(this.gridData);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LabelModule } from '@progress/kendo-angular-label';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridModule,
+    BrowserAnimationsModule, 
+    LabelModule,
+    InputsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
